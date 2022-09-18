7b2b72dd7dfb1f9402d743d3326d099f644e8ad8
Peter <peter.kotuliak@gmail.com>
2022-09-18 18:20:00 +0200

    first happy path failing test

--- test/ScoreBoardTest.java
+++ test/ScoreBoardTest.java
@@ -5,9 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class ScoreBoardTest {
 
     @Test
-    void testDummy()
+    void testHappyPath()
     {
         ScoreBoardImpl scoreBoard = new ScoreBoardImpl();
-        assertNull(scoreBoard.getSummary());
+        scoreBoard.startGame("England", "Russia");
+        scoreBoard.updateScore("England", 1, "Russia", 0);
+        assertEquals("England 1 - Russia 0", scoreBoard.getSummary());
+        scoreBoard.finishGame("England", "Russia");
+        assertEquals("No game in progress", scoreBoard.getSummary());
+
     }
 }
