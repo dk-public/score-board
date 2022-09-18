048787a1534a704d2999cf6ba4bf0b0bd9e81ffd
Peter <peter.kotuliak@gmail.com>
2022-09-18 20:05:24 +0200

    refactor of basic ordering implementation

--- src/sk/krajc/scoreboard/ScoreBoardImpl.java
+++ src/sk/krajc/scoreboard/ScoreBoardImpl.java
@@ -6,10 +6,13 @@ import sk.krajc.scoreboard.exception.NoSuchGameInProgressException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.function.IntSupplier;
 
 public class ScoreBoardImpl implements ScoreBoard {
 
     public static final String NO_GAME_IN_PROGRESS_MESSAGE = "No game in progress";
+    public enum Field { TOTAL_SCORE, START_TIME };
 
     private ArrayList<ScoreBoardRow> scoreBoardTable;
     private AtomicInteger timeOrdering; //not date type, because date not needed and might have insufficient time resolution in concurrent runs
@@ -55,18 +58,29 @@ public class ScoreBoardImpl implements ScoreBoard {
         }
     }
 
+    private int iterateUpUntilConditionMet(int iter, Field field, int condition){ // passing field because calling methods through reflection would be slow and complicated
+        while(iter >= 0 && (field == Field.START_TIME ? scoreBoardTable.get(iter).getStartTime(): scoreBoardTable.get(iter).getTotalScore()) < condition){
+            iter--;
+        }
+        return iter;
+    }
+
+    private int iterateUpUntilTotalScoreBigger(int iter, int score){
+        return iterateUpUntilConditionMet(iter, Field.TOTAL_SCORE, score);
+    }
+
+    private int iterateUpUntilStartTimeBigger(int iter, int time){
+        return iterateUpUntilConditionMet(iter, Field.START_TIME, time);
+    }
+
     private void ensureOrdering(String homeTeamName, String awayTeamName) {
         //doing sort from jdk library would have poor complexity (n log n or even more for 2 field sorting), so preferring manual maintenance
         int currentIndex = scoreBoardTable.indexOf(new ScoreBoardRow(homeTeamName, awayTeamName));
         int currentTotalScore = scoreBoardTable.get(currentIndex).updateTotalScore();
-        int iter = currentIndex - 1;
-        while(iter >= 0 && scoreBoardTable.get(iter).getTotalScore() < currentTotalScore){
-            iter--;
-        }
         int currentStartTime = scoreBoardTable.get(currentIndex).getStartTime();
-        while(iter >= 0 && scoreBoardTable.get(iter).getStartTime() < currentStartTime){
-            iter--;
-        }
+        int iter = currentIndex - 1;
+        iter = iterateUpUntilTotalScoreBigger(iter, currentTotalScore);
+        iter = iterateUpUntilStartTimeBigger(iter, currentStartTime);
         if(iter != currentIndex - 1){
             Collections.swap(scoreBoardTable, currentIndex, iter + 1);
         }
