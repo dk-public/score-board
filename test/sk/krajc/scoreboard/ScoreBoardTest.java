c09b97bf02542a6f4e4780e3bbc9898c3b80b2cf
Peter <peter.kotuliak@gmail.com>
2022-09-18 20:51:33 +0200

    additional edge test cases

--- test/sk/krajc/scoreboard/ScoreBoardTest.java
+++ test/sk/krajc/scoreboard/ScoreBoardTest.java
@@ -2,6 +2,7 @@ package sk.krajc.scoreboard;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import sk.krajc.scoreboard.exception.BadTeamNameException;
 import sk.krajc.scoreboard.exception.GameAlreadyInProgressException;
 import sk.krajc.scoreboard.exception.NoSuchGameInProgressException;
 
@@ -86,18 +87,67 @@ class ScoreBoardTest {
         scoreBoard.startGame("Argentina", "Australia");
         scoreBoard.updateScore("Argentina", 3, "Australia", 1);
 
-
-
         assertEquals(
                 "Uruguay 6 - Italy 6\n" +
                         "Spain 10 - Brazil 2\n" +
                         "Mexico 0 - Canada 5\n" +
                         "Argentina 3 - Australia 1\n" +
                         "Germany 2 - France 2", scoreBoard.getSummary());
-
-
-
-
-
     }
+
+    @Test
+    void testSummaryOrderingFromExcerciseExtended()
+    {
+        ScoreBoardImpl scoreBoard = new ScoreBoardImpl();
+
+        scoreBoard.startGame("Mexico", "Canada");
+        scoreBoard.updateScore("Mexico", 0, "Canada", 5);
+
+        scoreBoard.startGame("Spain", "Brazil");
+        scoreBoard.updateScore("Spain", 10, "Brazil", 2);
+
+        scoreBoard.startGame("Germany", "France");
+        scoreBoard.updateScore("Germany", 2, "France", 2);
+
+        scoreBoard.startGame("Uruguay", "Italy");
+        scoreBoard.updateScore("Uruguay", 6, "Italy", 6);
+
+        scoreBoard.startGame("Argentina", "Australia");
+        scoreBoard.updateScore("Argentina", 3, "Australia", 1);
+
+        scoreBoard.updateScore("Spain", 11, "Brazil", 2);
+        scoreBoard.startGame("India", "Switzerland");
+        scoreBoard.updateScore("India", 1, "Switzerland", 0);
+
+        assertEquals(
+                "Spain 11 - Brazil 2\n" +
+                        "Uruguay 6 - Italy 6\n" +
+                        "Mexico 0 - Canada 5\n" +
+                        "Argentina 3 - Australia 1\n" +
+                        "Germany 2 - France 2\n" +
+                        "India 1 - Switzerland 0", scoreBoard.getSummary());
+    }
+
+    @Test
+    void testExceptionOfNullTeamName()
+    {
+        ScoreBoardImpl scoreBoard = new ScoreBoardImpl();
+
+        Assertions.assertThrows(BadTeamNameException.class, () -> {
+            scoreBoard.startGame("England", null);
+        }, "BadTeamNameException was expected");
+    }
+
+    @Test
+    void testArithmeticException()
+    {
+        ScoreBoardImpl scoreBoard = new ScoreBoardImpl();
+
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            scoreBoard.startGame("Mexico", "Canada");
+            scoreBoard.updateScore("Mexico", 0, "Canada", 5);
+            scoreBoard.updateScore("Mexico", 0, "Canada", 0);
+        }, "ArithmeticException was expected");
+    }
+
 }
